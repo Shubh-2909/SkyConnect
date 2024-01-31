ee4926db3d3ef7cea8839b7010ec9e699099700a
Shubh Shubhanjal <shubhshubhanjal96@gmail.com>
2024-01-31 15:02:42 +0530

    Created flights crud

---
+++ src/services/flight-service.js
@@ -0,0 +1,42 @@
+const  {FlightRepository,AirplaneRepository}  = require('../repository/index');
+const {compareTime} = require('../utils/helper')
+
+class FlightService {
+
+    constructor() {
+        this.airplaneRepository = new AirplaneRepository();
+        this.flightRepository = new FlightRepository();
+    }
+
+    async createFlight(data) {
+        try {
+            if(!compareTime(data.arrivalTime ,  data.departureTime)) {
+                throw {error :'Arrival time must be earlier than departure time'} 
+            }
+            const airplane = await this.airplaneRepository.getAirplane(data.airplaneId);
+            const flight = await this.flightRepository.createFlight({ ...data, totalSeats: airplane.capacity });  // in data array ...data , this means we destructure the object and one more key named as totalSeats
+            return flight;
+        } catch (error) {
+            console.log("Something went wrong at service layer");
+            throw { error };
+        }
+    }
+
+    async getFlightData() {
+        // to do
+    }
+
+}
+
+module.exports = FlightService;
+
+/*
+  flightnumber,
+  airplaneId,
+  departureAirportId,
+  arrivalAirportId,
+  arrivalTime,
+  departureTime,
+  price
+  totalSeats -> from airplane repository
+*/
--- src/services/index.js
+++ src/services/index.js
@@ -1,4 +1,5 @@
 
 module.exports = {
-    CityService: require('./city-services')
+    CityService: require('./city-services'),
+    FlightService : require('./flight-service')
 };
